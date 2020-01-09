a2b61a5fda8ab5d31d40b1466bd2674412dee22f
Peter Rydell <peterydell@gmail.com>
2020-01-08 21:41:57 -0800

    initial app, db, config, model, controller nd views setup

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chili Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
