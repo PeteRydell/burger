b9ee19373afc91e0fe52079201c64e184dc28bd7
Peter Rydell <peterydell@gmail.com>
2020-02-09 22:15:53 -0800

    controller updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,16 +2,12 @@ var express = require("express");
 
 var router = express.Router();
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    burger.all(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
+    burger.selectAll(function(data) {
+      res.render("index", { burgers: data });
     });
   });
   
