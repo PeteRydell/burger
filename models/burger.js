cc65f10869394f9428735eb743dbac733ff3ea4a
Peter Rydell <peterydell@gmail.com>
2020-02-10 20:52:55 -0800

    burger.js model updates

--- models/burger.js
+++ models/burger.js
@@ -1,19 +1,23 @@
 var orm = require("../config/orm");
 
 var burger = {
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+    insertOne: function (cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res)
       });
-    },
-    insertOne: function (newDataObject, cb) {
-      orm.insertOne("burgers", newDataObject, cb);
   },
   // update an existing burger
-  updateOne: function (updatedDataObject, id, cb) {
-      orm.updateOne("burgers", updatedDataObject, id, cb);
+    updateOne: function (updatedObject, condition, cb) {
+      orm.updateOne("burgers", updatedObject, condition, function(res) {
+        cb(res)
+      });
   }
-  };
   
+};
   // Export the database functions for the controller (burgerController.js).
   module.exports = burger;
