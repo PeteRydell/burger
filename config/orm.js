d404e8de1e593f91fb316bcadbd3e6748cc529e6
Peter Rydell <peterydell@gmail.com>
2020-02-10 21:12:56 -0800

    orm updates

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,48 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection");
+
+function inputSql(ob) {
+  var arr = [];
+
+  for (let key in ob) {
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf("") >= 0){
+        value = "" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
 
 var orm = {
-  selectAll: function (tableName, cb) {
+  selectAll: function (tableInput, cb) {
     // creates sql query
-    var queryString = 'SELECT * FROM ??'
- 
-    connection.query(queryString, [tableName], function (err, result) {
+    var queryString = 'SELECT * FROM ' + tableInput + ";";
+    connection.query(queryString, function (err, result) {
         // if error throw err
-        if (err) throw err;
+        if (err) {
+          throw err;
+        }
         cb(result);
     });
   },
-  insertOne: function (tableName, newDataObject, cb) {
-    var queryString = 'INSERT INTO ?? SET ?';
-    connection.query(queryString, [tableName, newDataObject], function (err, result) {
-        if (err) throw err;
-        cb(result);
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = 'INSERT INTO ' + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += ") ";
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
     });
   },
+  
   updateOne: function (tableName, updatedDataObject, id, cb) {
     var queryString = 'UPDATE ?? SET ? WHERE id = ?'
     connection.query(queryString, [tableName, updatedDataObject, id], function (err, result) {
