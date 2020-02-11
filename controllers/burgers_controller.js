3c74ee2e5323cac1f517ffd2e0076f9fab4cde13
Peter Rydell <peterydell@gmail.com>
2020-02-10 21:01:49 -0800

    burgers controller updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,23 +7,34 @@ var burger = require("../models/burger");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
     burger.selectAll(function(data) {
-      res.render("index", { burgers: data });
+      var bObj = {
+        burgers: data
+      };
+      res.render("index", bObj);
     });
   });
   
-  router.post("/burgers/insert", function (req, res) {
-    console.log(req.body);
-
-    burger.insertOne(req.body, function (data) {
-        res.redirect("/");
+  router.post("/api/burgers", function (req, res) {
+    burger.insertOne([
+      "burger_name", "devoured"
+    ], [
+      req.body.burger_name, req.body.devoured = false
+    ], function (result) {
+      res.redirect("/")
     });
   });
   
-  router.put("/burgers/:id", function (req, res) {
-    // var updateBurger = { burger_name: "" };
-    burger.updateOne({ devoured: 1 }, req.params.id, function (data) {
-        res.sendStatus(200);
-    });
+  router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+    burger.updateOne({ 
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {req.params.id,
+        res.sendStatus(200).end();
+    }
+  });
   });
 
 // Export routes for server.js to use.
